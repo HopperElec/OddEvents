1b33d1879144b23585ebff84e5fd6fc168c91177
BuildTools <unconfigured@null.spigotmc.org>
2020-07-04 07:07:51 +0100

    - Fixed command - Added BlockBreakEvent - Fixed name

--- src/main/java/uk/co/hopperelec/mc/oddevents/Main.java
+++ src/main/java/uk/co/hopperelec/mc/oddevents/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,13 +13,23 @@ import java.util.Random;
 
 public class Main extends JavaPlugin implements Listener {
     Random random = new Random();
+    boolean toggled = true;
 
     @Override
     public void onEnable() {getServer().getPluginManager().registerEvents(this,this);}
 
     @Override
-    public boolean onCommand(CommandSender author, Command cmd, String label, String[] args) {HandlerList.unregisterAll();return true;}
+    public boolean onCommand(CommandSender author, Command cmd, String label, String[] args) {
+        if (toggled) {HandlerList.unregisterAll(); toggled = false;
+        } else {getServer().getPluginManager().registerEvents(this,this); toggled = true;}
+        return true;}
 
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) {if (random.nextInt(96) == 0) {event.setCancelled(true);}}
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (random.nextInt(96) == 0) {event.setCancelled(true);}}
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (random.nextBoolean()) {event.getBlock().setType(event.getBlock().getType());
+        } else {event.setDropItems(false);}}
 }
